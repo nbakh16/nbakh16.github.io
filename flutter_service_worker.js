7c2a8f7281f908db5a8d13795c30460765b4c701
nbakh16 <nbakh16@gmail.com>
2024-04-02 16:38:30 +0600

    responsive fix

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,14 +3,14 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"main.dart.js": "dbbe11160fbcec6c9feea881e31384fb",
+const RESOURCES = {"main.dart.js": "65f7a63634d91046da8e35ce9c9758ac",
 "flutter.js": "c71a09214cb6f5f8996a531350400a9a",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
-"index.html": "5f18c9980674b9ae93e1a24443ab90d7",
-"/": "5f18c9980674b9ae93e1a24443ab90d7",
+"index.html": "508b03da9feb5d4178fe3b3f7ba1ce27",
+"/": "508b03da9feb5d4178fe3b3f7ba1ce27",
 "canvaskit/skwasm.js.symbols": "741d50ffba71f89345996b0aa8426af8",
 "canvaskit/skwasm.wasm": "e42815763c5d05bba43f9d0337fa7d84",
 "canvaskit/chromium/canvaskit.wasm": "f5934e694f12929ed56a671617acd254",
